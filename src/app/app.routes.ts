54cd55fc26eeb9d2e13e414d4157bd34c2a388a8
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-03-30 20:43:30 +0530

    Learned Basic Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'welcome', component: WelcomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path : '**' , component: ErrorComponent}
+];
