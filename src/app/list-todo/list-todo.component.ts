403e5ff9ef937608c26c553d9ce2218493fd8afe
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-04-18 11:43:47 +0530

    Keeping Todo class as separate model
    * Using the Todo object to set the list of todos in component.ts

--- src/app/list-todo/list-todo.component.ts
+++ src/app/list-todo/list-todo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DatePipe, NgFor, NgIf } from '@angular/common';
+import { Todo } from '../list-todo/list-todo.model';
 
 @Component({
   selector: 'app-list-todo',
@@ -8,23 +9,15 @@ import { DatePipe, NgFor, NgIf } from '@angular/common';
   styleUrl: './list-todo.component.css'
 })
 
-// export class Todo{
-//   constructor(
-//     public id: number,
-//     public description: string,
-//     public done :boolean,
-//     public targetDate : Date
-//   ) { }
-// }
 export class ListTodoComponent {
 
   todos = [
-    // new Todo(1, 'Learn To Dance', false, new Date()),
-    // new Todo(2, 'Learn To Fly', false, new Date()),
-    // new Todo(3, 'Learn To run', false, new Date())
-    { id: 1, description : 'Learn To Dance', done: false, targetDate: new Date()},
-    { id: 2, description : 'Learn To Fly', done: false, targetDate: new Date()}, 
-    { id: 3, description : 'Learn To run', done: false, targetDate: new Date()}
+    new Todo(1, 'Learn To Dance', false, new Date()),
+    new Todo(2, 'Learn To Fly', false, new Date()),
+    new Todo(3, 'Learn To run', false, new Date())
+    // { id: 1, description : 'Learn To Dance', done: false, targetDate: new Date()},
+    // { id: 2, description : 'Learn To Fly', done: false, targetDate: new Date()}, 
+    // { id: 3, description : 'Learn To run', done: false, targetDate: new Date()}
   ]
 
   // todo = {id: 1, description : 'Learn To Dance'}
