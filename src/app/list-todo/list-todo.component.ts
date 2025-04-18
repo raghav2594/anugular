1c6efa6e5054f661c0cb14b233ccab1b72078b34
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-04-18 11:38:28 +0530

    Showing date in todo-list

--- src/app/list-todo/list-todo.component.ts
+++ src/app/list-todo/list-todo.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
-import { DatePipe, NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { DatePipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-list-todo',
-  imports: [NgIf, NgFor, UpperCasePipe, DatePipe],
+  imports: [NgIf, NgFor, DatePipe],
   templateUrl: './list-todo.component.html',
   styleUrl: './list-todo.component.css'
 })
+
+// export class Todo{
+//   constructor(
+//     public id: number,
+//     public description: string,
+//     public done :boolean,
+//     public targetDate : Date
+//   ) { }
+// }
 export class ListTodoComponent {
 
-  todos = [{ id: 1, description : 'Learn To Dance'},
-    { id: 2, description : 'Learn To Fly'},
-    { id: 3, description : 'Learn To run'}
+  todos = [
+    // new Todo(1, 'Learn To Dance', false, new Date()),
+    // new Todo(2, 'Learn To Fly', false, new Date()),
+    // new Todo(3, 'Learn To run', false, new Date())
+    { id: 1, description : 'Learn To Dance', done: false, targetDate: new Date()},
+    { id: 2, description : 'Learn To Fly', done: false, targetDate: new Date()}, 
+    { id: 3, description : 'Learn To run', done: false, targetDate: new Date()}
   ]
 
   // todo = {id: 1, description : 'Learn To Dance'}
