f1c17e0ccbf87d725bc558950bdc19a010d88548
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-04-23 14:26:54 +0530

    Use BootStrap to for better visibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
     WelcomeComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
